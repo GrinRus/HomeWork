92d052e7e291376586c284507df106378da117f9
Grigorii Riabov <uhbif91@mail.ru>
2022-09-07 18:36:05 +0400

    home work 2 task1

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
+﻿    internal class Task1
+
     {
-     
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+
+            string input = Console.ReadLine();
+
+            int parsedInput = int.Parse(input);
+
+            if (parsedInput % 2 == 1)
+            {
+                Console.WriteLine("Нечетное число");
+            }
+            else
+            {
+                Console.WriteLine("Четно число");
+            }
+        }
     }
-}
