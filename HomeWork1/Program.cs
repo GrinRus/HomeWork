01a7a295d935c525f9d9b39aaf832e133534f925
Grigorii Riabov <uhbif91@mail.ru>
2022-09-07 14:42:06 +0400

    complete

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string fullName = "Grigorii Riabov";
+        int age = 30;
+        string email = "some@mail.com";
+        double programmingScore = 3.5;
+        double mathScore = 4.0;
+        double physicsScore = 1.1;
+
+        Console.WriteLine($"Полное имя: {fullName}  Возраст: {age}  Емейл: {email} Баллы по программированию: {programmingScore} Баллы по математике: {mathScore} Баллы по физике: {physicsScore} ");
+
+        Console.ReadLine();
+
+        double scoreSumm = programmingScore + mathScore + physicsScore;
+
+        double scoreAverage = scoreSumm / 3;
+
+        Console.WriteLine($"Сумма баллов: {scoreSumm:#.##}  Среднее арифметическое баллов: {scoreAverage:#.##}");
+    }
+}
