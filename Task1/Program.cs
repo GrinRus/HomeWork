4fbf82fee6b665fd7485f7a689a8b57fca6ec179
Grigorii Riabov <uhbif91@mail.ru>
2022-09-28 16:28:17 +0400

    complete hw4

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите текст");
+
+        string input = Console.ReadLine();
+
+        string[] words = GetWordsFromInput(input);
+
+        PrintWords(words);
+    }
+
+    private static void PrintWords(string[] words)
+    {
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+    private static string[] GetWordsFromInput(string lines)
+    {
+        return lines.Split(' ');
+    }
+}
