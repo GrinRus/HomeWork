c9ff0d42835c11d71bbefcbffb8543dea17002d2
Grigorii Riabov <uhbif91@mail.ru>
2022-09-28 16:07:08 +0400

    complete HW 3

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Сколько строк в матрице");
+
+        int lines = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Сколько столбцом в матрице");
+
+        int columns = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[lines, columns];
+
+        Random random = new Random();
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i,j] = random.Next(100);
+            }
+        }
+
+        long sum = 0;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int var = matrix[i, j];
+                sum += var;
+                Console.Write($"{var, 3}");
+            }
+            Console.WriteLine();
+        }
+
+        Console.Write($"Сумма матрицы {sum}");
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Сколько строк в матрице");
+
+        int lines = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Сколько столбцом в матрице");
+
+        int columns = int.Parse(Console.ReadLine());
+
+        int[,] matrix1 = new int[lines, columns];
+        int[,] matrix2 = new int[lines, columns];
+        int[,] resMatrix = new int[lines, columns];
+
+        Random random = new Random();
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                matrix1[i, j] = random.Next(100);
+            }
+        }
+
+        for (int i = 0; i < matrix2.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                matrix2[i, j] = random.Next(100);
+            }
+        }
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                int var1 = matrix1[i, j];
+                int var2 = matrix2[i, j];
+                resMatrix[i,j] = var1 + var2;
+            }
+        }
+
+        for (int i = 0; i < resMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resMatrix.GetLength(1); j++)
+            {
+                Console.Write($"{resMatrix[i, j], 4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
